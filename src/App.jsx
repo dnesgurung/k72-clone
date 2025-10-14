266d15db6bee6fab09b5fc8c26e35c533f983860
dnesgurung <174604785+dnesgurung@users.noreply.github.com>
2025-10-14 11:09:14 +0100

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import { Link, Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import Agence from './pages/Agence'
+import Projects from './pages/Projects'
+
+const App = () => {
+  return (
+    <div className='text-white'>
+      <Link className="text-blue-500 mr-5 text-sm" to='/'>Home</Link>
+      <Link className="text-blue-500 mr-5 text-sm" to='/agence'>Agence</Link>
+      <Link className="text-blue-500 mr-5 text-sm" to='/projects'>Projects</Link>
+
+      <Routes>
+        <Route path='/' element = {<Home/>}/>
+        <Route path='/agence' element = {<Agence/>}/>
+        <Route path='/projects' element = {<Projects/>}/>
+      </Routes>
+    </div>
+  )
+}
+
+export default App
---
+++ src/pages/Agence.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Agence = () => {
+  return (
+    <div>Agents</div>
+  )
+}
+
+export default Agence
