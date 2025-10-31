0cbf4d98fefea77d6a6d1c334ee1ff845b3f5553
dnesgurung <174604785+dnesgurung@users.noreply.github.com>
2025-10-31 22:44:36 +0000

    ..

--- src/components/navigation/FullScreenNav.jsx
+++ src/components/navigation/FullScreenNav.jsx
@@ -4,11 +4,11 @@ const FullScreenNav = () => {
   return (
     <div id="fullScreenNav" className="h-screen w-full absolute bg-amber-900">
       <div id="all-links" className="mt-[10vw]">
-        <div className="link border-t-1">
+        <div className="relative link border-t-1">
           <h1 className="font-[mainFont500] text-[8vw] text-center leading-[0.8] pt-5 uppercase">
             Projets
           </h1>
-          <div className="absolute flex">
+          <div className="absolute flex top-0 bg-[#D3FD50]">
             <div className="flex overflow-x-auto items-center ">
               <h2 className="whitespace-nowrap font-[mainFont500] text-[8vw] text-center leading-[0.8] pt-5 uppercase">
                 Pour Tout Voir
